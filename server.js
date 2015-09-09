8e75a7addf65671a0c21ae939273ec6a5e4d4727
sherlocktech <sean.stamm5@gmail.com>
2015-09-09 13:27:46 -0400

    Add support of YQL and for MySQL

--- server.js
+++ server.js
@@ -2,17 +2,27 @@
 	"use strict";
 
 	var express = require('express'),
-		sql = require('sql'),
+		mysql = require('mysql'),
+		yql = require('yql-node'),
 		app = express();
 
-	//define sql tables
-	var stock = sql.define({
-		name: 'stock',
-		columns: ['tickSym', 'name']
+	//connect node mysql to mysql server
+	//info required is stored in object
+	var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'sherlocktech',
+		password : 'ssta8767',
+		database : 'FAT'
 	});
-	var price = sql.define({
-		name: 'price',
-		columns: ['tickSym', 'openPrice', 'closePrice']
-	});
+	connection.connect();
 
+	//use YQL to return JSON from a csv on Quandl
+	//prints JSON to console
+	var yqueryAAPL = 'select * from csv where url="https://www.quandl.com/api/v1/datasets/WIKI/AAPL.csv?auth_token=HWxsDbuyfaaxHtq4wLUi"';
+	yql.execute(yqueryAAPL, function(err, response) {
+		if(!err){
+			console.log(response);
+		} else {
+			console.error(err);
+		}
 } ());
