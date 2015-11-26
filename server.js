247a48ad6c2623860d7fec11f3d76e6af9da9e9a
sherlocktech <sean.stamm5@gmail.com>
2015-11-26 15:20:44 -0500

    Add working AAPL current assets and liabilities request: still need to deal with async requests and process data

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 (function () {
 	"use strict";
-
+	//app variables
 	var express = require('express'),
-		mysql = require('mysql'),
-		yql = require('yql-node'),
-		app = express();
+		  mysql = require('mysql'),
+			https = require('https'),
+		  app = express();
 
 	app.use(express.static(__dirname));
+
+	//query variables
+	var query = '',
+		  quandlURI = 'https://www.quandl.com/api/v3/datasets/',
+			fundamentalsCode = 'SEC/',
+			priceCode = 'WIKI/',
+			companyName = '',
+			AAPL_DIV = 'SEC/DIV_AAPL.json',
+			AAPL_ASSETS = 'SEC/AAPL_ASSETSCURRENT_A.json',
+			AAPL_LIABILITIES = 'SEC/AAPL_LIABILITIESCURRENT_A.json',
+			apiKey = '?api_key=NxE-gV7J35m1xs-TEsrq';
 
 	//connect node mysql to mysql server
 	//info required is stored in object
@@ -14,7 +25,7 @@
 		host     : 'localhost',
 		user     : 'sherlocktech',
 		password : 'ssta8767',
-		database : 'FAT'
+		database : 'fat'
 	});
 	connection.connect();*/
 
@@ -25,14 +36,52 @@
 		console.log('Server listening at http://%s:%s', host, port);
 	});
 
-	//use YQL to return JSON from a csv on Quandl
-	//prints JSON to console
-	var yqueryAAPL = 'select * from csv where url="https://www.quandl.com/api/v1/datasets/WIKI/AAPL.csv?auth_token=HWxsDbuyfaaxHtq4wLUi"';
-	yql.execute(yqueryAAPL, function(err, response) {
-		if(!err){
-			console.log(response);
-		} else {
-			console.error(err);
-		}
+	var r;
+
+	app.get('/', function (req, res) {
+		res.send(r);
 	});
+
+	//request AAPL current assest (annual)
+	console.log('Requesting AAPL current assest (annual)');
+	query = quandlURI + AAPL_ASSETS + apiKey;
+	var assetReq = https.get(query, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		// Buffer the body entirely for processing as a whole.
+		var bodyChunks = [];
+		res.on('data', function(chunk) {
+			// You can process streamed parts here...
+			bodyChunks.push(chunk);
+		}).on('end', function() {
+			var body = Buffer.concat(bodyChunks);
+			console.log('BODY: ' + body);
+			// ...and/or process the entire body here.
+		})
+	});
+	assetReq.on('error', function(e) {
+		console.log('ERROR: ' + e.message);
+	});
+
+	//request AAPL current liabilities (annual)
+	console.log('Requesting AAPL current liabilties (annual)');
+	query = quandlURI + AAPL_LIABILITIES + apiKey;
+	var liabilityReq = https.get(query, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		// Buffer the body entirely for processing as a whole.
+		var bodyChunks = [];
+		res.on('data', function(chunk) {
+			// You can process streamed parts here...
+			bodyChunks.push(chunk);
+		}).on('end', function() {
+			var body = Buffer.concat(bodyChunks);
+			console.log('BODY: ' + body);
+			// ...and/or process the entire body here.
+		})
+	});
+	liabilityReq.on('error', function(e) {
+		console.log('ERROR: ' + e.message);
+	});
+
 } ());
