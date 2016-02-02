350f9deb1e84e01e9140d545587d65af3fb75f65
Sean Stamm <sean.stamm5@gmail.com>
2016-02-02 13:56:14 -0800

    Starting fresh on this branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# fundamental-analysis-teacher
-This is a CS senior project with the goal of teaching users of the website how to conduct fundamental analysis of stocks and potentially any financial asset.
--- client.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	var query = '';
-	$(".submitForm button").click(function() {
-		//build query string
-		query = $("#tickerSymbol").val();
-		//get request from express server possibly via angular
-        $.get('data');
-	});
-}());
