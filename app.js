fc3897f50fb1e5586197e1554f8df4a69184d505
sherlocktech <seanstamm@Christinas-MacBook-Pro.local>
2016-04-06 15:51:16 -0700

    Add stock history from last 30ish days
    Compute the date for one month before now, query Quandl based on that
    date rather than a static date range.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //define app
 var main = function(){
+  //create date object as current time, based on client system clock
+  var currentTime = new Date();
   //when button is clicked build the stock price history chart
   $('#search .btn').click( function() {
     var query = $( '#company-search' ).val();
-    buildStockHistoryChart( query );
+    buildStockHistoryChart( query, currentTime );
+    //fillFundamentalsTable( query, currentTime );
   });
 };
 
