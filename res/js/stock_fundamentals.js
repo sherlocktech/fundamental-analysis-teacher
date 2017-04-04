96a0b38f82d1d924108d1297c52ffb70d4f05f8e
Sean Stamm <sean.stamm5@gmail.com>
2017-04-04 11:14:36 -0700

    Delete entire app

--- res/js/stock_fundamentals.js
+++
@@ -1,7 +0,0 @@
-/*
-    Builds company fundamentals section of Dashboard based on given
-    fundamentals object.
-*/
-var buildFundamentalsTable = function (fundamentals) {
-  $('#results .stock_fundamentals div').html('<table class="table table-hover"><tr><th>Current Ratio</th><td>1.11</td><th>Quick Ratio</th><td>1.08</td><th>Profit Margin</th><td>0.23</td></tr>');
-}
